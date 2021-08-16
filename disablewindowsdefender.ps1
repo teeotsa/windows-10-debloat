bdf3aaab7b4277f90364d69f9fc8a3f02baecbb6
teeotsa <78772453+teeotsa@users.noreply.github.com>
2021-08-16 12:59:03 -0700

    Made script better

--- disablewindowsdefender.ps1
+++ disablewindowsdefender.ps1
@@ -1,9 +1,22 @@
-.\x64\NSudoLG.exe -U:T -P:E -ShowWindowMode:Hide powershell.exe Set-Service -StartupType Disabled "WinDefend"
-.\x64\NSudoLG.exe -U:T -P:E -ShowWindowMode:Hide powershell.exe Stop-Service -Force -Name "WinDefend"
-.\x64\NSudoLG.exe -U:T -P:E -ShowWindowMode:Hide powershell.exe Set-Service -StartupType Disabled "WdNisSvc"
-.\x64\NSudoLG.exe -U:T -P:E -ShowWindowMode:Hide powershell.exe Stop-Service -Force -Name "WdNisSvc"
-.\x64\NSudoLG.exe -U:T -P:E -ShowWindowMode:Hide powershell.exe Set-Service -StartupType Disabled "mpssvc"
-.\x64\NSudoLG.exe -U:T -P:E -ShowWindowMode:Hide powershell.exe Stop-Service -Force -Name "mpssvc"
-.\x64\NSudoLG.exe -U:T -P:E -ShowWindowMode:Hide powershell.exe Set-Service -StartupType Disabled "Sense"
-.\x64\NSudoLG.exe -U:T -P:E -ShowWindowMode:Hide powershell.exe Stop-Service -Force -Name "Sense"
-exit
+$Path = Get-Location
+$Cmds = @(
+    "powershell.exe Set-Service -StartupType Disabled 'WinDefend' -ErrorAction SilentlyContinue"
+    "powershell.exe Stop-Service -Force -Name 'WinDefend' -ErrorAction SilentlyContinue"
+    "powershell.exe Set-Service -StartupType Disabled 'WdNisSvc' -ErrorAction SilentlyContinue"
+    "powershell.exe Stop-Service -Force -Name 'WdNisSvc' -ErrorAction SilentlyContinue"
+    "powershell.exe Set-Service -StartupType Disabled 'mpssvc' -ErrorAction SilentlyContinue"
+    "powershell.exe Stop-Service -Force -Name 'mpssvc' -ErrorAction SilentlyContinue"
+    "powershell.exe Set-Service -StartupType Disabled 'Sense' -ErrorAction SilentlyContinue"
+    "powershell.exe Stop-Service -Force -Name 'Sense' -ErrorAction SilentlyContinue"
+)
+
+if(Test-Path "$Path\x64"){
+    foreach ($Cmds in $Cmds) {
+        .\x64\NSudoLG.exe -U:T -P:E -ShowWindowMode:Hide $Cmds 
+    }
+    exit
+} else {
+    write-Host "NSudo folder not found! Please make sure you have 'x64' folder in your debloater folder!"
+    timeout 5
+    exit
+}
