fa91e53440ef633c965cbe717f28296e01b595ce
teeotsa <78772453+teeotsa@users.noreply.github.com>
2021-08-25 11:17:03 -0700

    Fixed and made it better

--- disablewindowsdefender.ps1
+++ disablewindowsdefender.ps1
@@ -1,22 +1,23 @@
-$Path = Get-Location
-$Cmds = @(
-    "powershell.exe Set-Service -StartupType Disabled 'WinDefend' -ErrorAction SilentlyContinue"
-    "powershell.exe Stop-Service -Force -Name 'WinDefend' -ErrorAction SilentlyContinue"
-    "powershell.exe Set-Service -StartupType Disabled 'WdNisSvc' -ErrorAction SilentlyContinue"
-    "powershell.exe Stop-Service -Force -Name 'WdNisSvc' -ErrorAction SilentlyContinue"
-    "powershell.exe Set-Service -StartupType Disabled 'mpssvc' -ErrorAction SilentlyContinue"
-    "powershell.exe Stop-Service -Force -Name 'mpssvc' -ErrorAction SilentlyContinue"
-    "powershell.exe Set-Service -StartupType Disabled 'Sense' -ErrorAction SilentlyContinue"
-    "powershell.exe Stop-Service -Force -Name 'Sense' -ErrorAction SilentlyContinue"
-)
-
-if(Test-Path "$Path\x64"){
-    foreach ($Cmds in $Cmds) {
-        .\x64\NSudoLG.exe -U:T -P:E -ShowWindowMode:Hide $Cmds 
-    }
+if (Test-Path ".\x64"){
+	
+	write-Host "NSudo folder found! Script will start now!"
+	    .\x64\NSudoLG.exe -U:T -P:E -ShowWindowMode:Hide powershell "Set-Service -StartupType Disabled 'WinDefend' -ErrorAction SilentlyContinue"
+        .\x64\NSudoLG.exe -U:T -P:E -ShowWindowMode:Hide powershell "Stop-Service -Force -Name 'WinDefend' -ErrorAction SilentlyContinue"
+        .\x64\NSudoLG.exe -U:T -P:E -ShowWindowMode:Hide powershell "Set-Service -StartupType Disabled 'WdNisSvc' -ErrorAction SilentlyContinue"
+        .\x64\NSudoLG.exe -U:T -P:E -ShowWindowMode:Hide powershell "Stop-Service -Force -Name 'WdNisSvc' -ErrorAction SilentlyContinue"
+        .\x64\NSudoLG.exe -U:T -P:E -ShowWindowMode:Hide powershell "Set-Service -StartupType Disabled 'mpssvc' -ErrorAction SilentlyContinue"
+        .\x64\NSudoLG.exe -U:T -P:E -ShowWindowMode:Hide powershell "Stop-Service -Force -Name 'mpssvc' -ErrorAction SilentlyContinue"
+        .\x64\NSudoLG.exe -U:T -P:E -ShowWindowMode:Hide powershell "Set-Service -StartupType Disabled 'Sense' -ErrorAction SilentlyContinue"
+        .\x64\NSudoLG.exe -U:T -P:E -ShowWindowMode:Hide powershell "Stop-Service -Force -Name 'Sense' -ErrorAction SilentlyContinue"
+    Clear-Host
+    write-Host "Done, script will close in 3 seconds"
+    Start-Sleep -Seconds 3
     exit
+	
 } else {
-    write-Host "NSudo folder not found! Please make sure you have 'x64' folder in your debloater folder!"
-    timeout 5
+	
+	write-Host "NSudo folder not found! Script will close in 3 seconds"
+	Start-Sleep -Seconds 3
     exit
+	
 }
